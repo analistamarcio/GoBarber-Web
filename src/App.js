3d85ba1e1859641de697a916dffa06626545be80
Marcio Ferreira <analistamarcio@hotmail.com>
2020-01-26 13:04:03 -0800

    Setting Reactotron

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 
-import history from 'history';
+import './config/Reactotron.Config';
+
 import Routes from './routes';
+import history from './services/history';
 
 function App() {
   return (
