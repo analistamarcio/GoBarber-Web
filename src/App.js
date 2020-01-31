38a7925a5316b059bc793142bd97e237da5c0be3
Marcio Ferreira <analistamarcio@hotmail.com>
2020-01-30 19:21:10 -0800

    Auth persist

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
@@ -7,17 +8,19 @@ import './config/Reactotron.Config';
 import Routes from './routes';
 import history from './services/history';
 
-import store from './store'; // must be after imports Reactotron library
+import { store, persistor } from './store'; // must be after imports Reactotron library
 
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyle />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
